7a237ca7e2845e6fd68843a3078fffca68a832aa
PortadordeCNPJ <guilhermedesouzamuller@gmail.com>
2024-02-06 11:30:05 -0300

    add: Adicionando o composer

--- bootstrap.php
+++ bootstrap.php
@@ -3,6 +3,5 @@
 session_start();
 
 
-require "../app/functions/pages.php";
-require "../../../app/functions/custom.php";
-require "../../../app/functions/validate.php";
+require "vendor/autoload.php";
+
